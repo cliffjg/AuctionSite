6e8ca3a85d9c4e54e3222de5af4a379beafce430
cliffjg <162375679+cliffjg@users.noreply.github.com>
2024-03-08 18:25:04 -0500

    cleaned up Login.java and added comments

--- src/main/java/userpackage/Login.java
+++ src/main/java/userpackage/Login.java
@@ -45,20 +45,7 @@ public class Login extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-//		HttpSession session = request.getSession();
-//		
-//		session = request.getSession();
-//		
-//		String userEmail = (String)session.getAttribute("email");
-//		
-//		System.out.println(userEmail);
-		
-		
-//		String userEmail = request.getParameter("email");
-//		String userPassword = request.getParameter("pass");
-//		
-//		System.out.println("This is Email: " + userEmail);
-//		System.out.println("This is Password: " + userPassword);
+
 		
 		
 	}
@@ -67,34 +54,26 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		doGet(request, response);
-		
-		
-//		
-//		session = request.getSession();
-		
 
+		//Arrays for storing the auction objects
 		ArrayList<Auction> myAuction = new ArrayList();
 		ArrayList<Auction> communityAuction = new ArrayList();
 		ArrayList<Auction> myBids = new ArrayList();
 		
-//		
-		String myAuctions = "";
-		String myCommunity = "";
-//		String myBids ="";
-		String data = "";
+
+		//Getting the userEmail and Password form the input box
 		String userEmail = request.getParameter("userEmail");
 		String userPassword = request.getParameter("pass");
 		
-//		String query = "select userEmail, password from Users where userEmail = '" 
-//				+ userEmail + "'" + "and password = '" + userPassword +"';";
 		
+		//String for sql query to check if field is in database
 		String query = "select * from Users where userEmail = '" 
 				+ userEmail + "'" + "and password = '" + userPassword +"';";
 		
 
 		try {
+			
+			
 			DatabaseAccess db = new DatabaseAccess();
 			Connection connection = db.getConnection();
 			
@@ -103,98 +82,61 @@ public class Login extends HttpServlet {
 			ResultSet rs = statement.executeQuery(query);
 			
 			
+			//if Result set returns null or is empty the user
+			//is not in the database
+			//else if admin -> go to admin page
 			if(!(rs.next())) {
             	
 				
-				System.out.println("The user is NOT in the list! False\n");
-				System.out.println("!User: " + userEmail + "\n");
+				System.out.println("The user " + userEmail + "is NOT in the list! False\n");
 				
+				//send error message to userPageTest.jsp
             	request.setAttribute("success", "error");
             	request.getRequestDispatcher("loginerror.jsp").forward(request,response);
 
             	
             }else {
 
-            	
+            	//checks if either admin or User
             	if(userEmail.equals("admin@gmail.com")) {
             		
-            		
+            		//set the session and if can be retrieved when forwarded other page
             		HttpSession session = request.getSession();
 					session.setAttribute("userEmail", userEmail);
 					
-					System.out.println("!User: " + userEmail + "\n");
-            		//Forward to the servlet AdminPage.java 
+            		
+					//Forward to the servlet AdminPage.java 
             		RequestDispatcher dispatcher = request.getRequestDispatcher("/AdminPage");
             		dispatcher.forward(request, response);
             		
-            		
-//            		HttpSession session1 = request.getSession();
-//            		
-//            		//Send admin@gmail to message page
-//            		session1.setAttribute("userEmail", userEmail);
-//            		
-//            		RequestDispatcher dispatcher = request.getRequestDispatcher("/MessagePage");
-//            		dispatcher.forward(request, response);
 
             	}else {
-                	ResultSet rs1 = statement.executeQuery(query);
-                	//gets number of columns from the query 
+                	
+            		
+            		ResultSet rs1 = statement.executeQuery(query);
+                	
+            		//gets number of columns from the query 
         			//for for loop
         			ResultSetMetaData metadata = rs1.getMetaData();
-        			
-        			
-//        			int numberOfColumns = metadata.getColumnCount();
-//                	
-//                	while(rs1.next()) {
-//        				
-//        				for(int i = 1; i < numberOfColumns+1; i++) {
-//        					data += rs1.getString(i) + " ";
-//        				}
-//        				
-//        				
-//        				data += "\n";
-//        				
-//        			}
+
                 	
                 	String query2 = "select * from Auction where userEmail = '" 
             				+ userEmail + "';";
                 	
                 	ResultSet rs2 = statement.executeQuery(query2);
+                	
                 	//gets number of columns from the query 
         			//for for loop
         			ResultSetMetaData metadata2 = rs2.getMetaData();
+
         			
-        			int count = 1; 
+//        			int numberOfColumns2 = metadata2.getColumnCount();
         			
-        			
-        			int numberOfColumns2 = metadata2.getColumnCount();
-        			
-        			
-        			
-//        			Auction auction = new Auction();
-                	
-                	
-        			
+        		
         			
         			while(rs2.next()) {
                 		
-                		
-                		String myAuctions2 = null;
-                		
-                		
-                		
-        				
-//        				for(int i = 1; i < numberOfColumns2+1; i++) {
-//        					System.out.println("ok: " + myAuctions2);
-    //
-//        					
-//        					myAuctions2 += rs2.getString(i) + " ";
-//        					count++;
-//        				}
-        				
-//        			arrayList.add(myAuctions2);	
-        				
-                		
+   
         				Auction auction = new Auction(
              					rs2.getString("auctionID"),
              					rs2.getString("userEmail"),
@@ -214,64 +156,30 @@ public class Login extends HttpServlet {
         				auction.setCurrentBid(rs2.getString("currentBid"));
     					auction.setBidderEmail(rs2.getString("bidderEmail"));
         				
-        				
-        				
-        				
-                		
-        				
-//        				arrayListAuction.add(auction);	
+
+    					//add the auction object to the myAuction arrayList
+    					//so and can be sent later
         				myAuction.add(auction);	
         				
 
         				
         			}
         			
-//        			for(int i = 0; i < myAuction.size(); i++){
-//        				System.out.println(myAuction.get(i).auctionID);
-//        				System.out.println(myAuction.get(i).userEmail);
-//        				System.out.println(myAuction.get(i).carImage);
-//        				System.out.println(myAuction.get(i).carYear);
-//        				System.out.println(myAuction.get(i).carMake);
-//        				System.out.println(myAuction.get(i).carModel);
-//        				System.out.println(myAuction.get(i).carColor);
-//        				System.out.println(myAuction.get(i).startingBid);
-//        				System.out.println(myAuction.get(i).carPrice);
-//        				System.out.println(myAuction.get(i).startDate);
-//        				System.out.println(myAuction.get(i).expirationDate);
-//        				System.out.println(myAuction.get(i).carDescription);
-//        				System.out.println(myAuction.get(i).imagePath);
-//        				
-//        				System.out.println("\n");
-//        			}
-//        			
-        			
-                	
-//                	System.out.println(arrayList);
-        			
-//        			for(String list: auction)
-//        			
-//        			System.out.println(arrayListAuction);
-                	
+
+                	//Run query to return all auctions
                 	String query3 = "select * from Auction;";
                 	
                 	ResultSet rs3 = statement.executeQuery(query3);
+                	
+                	
                 	//gets number of columns from the query 
         			//for for loop
-        			ResultSetMetaData metadata3 = rs3.getMetaData();
+//        			ResultSetMetaData metadata3 = rs3.getMetaData();
         			
         			
-        			int numberOfColumns3 = metadata3.getColumnCount();
                 	
                 	while(rs3.next()) {
-        				
-//        				for(int i = 1; i < numberOfColumns3+1; i++) {
-//        					myCommunity += rs3.getString(i) + " ";
-//        				}
-        				
-        				
-//        				data += "\n";
-                		
-                		
+        				      		
                 		
                 		Auction auction = new Auction(
              					rs3.getString("auctionID"),
@@ -292,34 +200,12 @@ public class Login extends HttpServlet {
         				
                 		auction.setCurrentBid(rs3.getString("currentBid"));
     					auction.setBidderEmail(rs3.getString("bidderEmail"));
-        				
-        				
-                		
-        				
-//        				community.add(auction);
+
+    					
+    					//add the auction object to the communityAuction arrayList
+    					//so and can be sent later
         				communityAuction.add(auction);
-        				
-        				
-//        				for(int i = 0; i < communityAuction.size(); i++){
-//            				System.out.println(communityAuction.get(i).auctionID);
-//            				System.out.println(communityAuction.get(i).userEmail);
-//            				System.out.println(communityAuction.get(i).carImage);
-//            				System.out.println(communityAuction.get(i).carYear);
-//            				System.out.println(communityAuction.get(i).carMake);
-//            				System.out.println(communityAuction.get(i).carModel);
-//            				System.out.println(communityAuction.get(i).carColor);
-//            				System.out.println(communityAuction.get(i).startingBid);
-//            				System.out.println(communityAuction.get(i).carPrice);
-//            				System.out.println(communityAuction.get(i).startDate);
-//            				System.out.println(communityAuction.get(i).expirationDate);
-//            				System.out.println(communityAuction.get(i).carDescription);
-//            				System.out.println(communityAuction.get(i).imagePath);
-//            				
-//            				System.out.println("\n");
-//            			}
-                		
-                		
-                		
+        				     		
                 		
                 		
         				
@@ -333,28 +219,18 @@ public class Login extends HttpServlet {
                 	
                 	
                 	HttpSession session = request.getSession();
-//                	session = request.getSession();
-//                    String username = (String)request.getAttribute("un");
-//                    session.setAttribute("UserName", username);
-        			
+
                 	response.sendRedirect("userpageTest.jsp");
-//                	session.setAttribute("email", userEmail);
+
+                	//set all the attributes to be forwarded to userPage.jsp
                 	session.setAttribute("userEmail", userEmail);
-//                	session.setAttribute("myAuctions", myAuctions);
-//                	session.setAttribute("myCommunity", myCommunity);
-//                	session.setAttribute("arrayList", arrayList);
-//                	session.setAttribute("arrayListAuction", arrayListAuction);
                 	session.setAttribute("myAuction", myAuction);
                 	session.setAttribute("communityAuction", communityAuction);
-        			
-        			
-                	
-                
                 
             	}
             	
 
-}
+            }
 			
 			
 			 db.closeConnection(connection);
